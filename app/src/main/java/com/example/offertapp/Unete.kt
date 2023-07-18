c4c07ab802b96826dbbcd93400fdb0cd2eadccbe
SrCosm <jalonsoaa2004@gmail.com>
2023-07-17 21:29:37 -0500

    Asd

---
+++ app/src/main/java/com/example/offertapp/Unete.kt
@@ -0,0 +1,11 @@
+package com.example.offertapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Unete : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unete)
+    }
+}
